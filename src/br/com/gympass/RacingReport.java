72a7dbae7cb71181f0803436a4343a3922ac9ded
Felipe Santos Pinheiro <pinheirofs@gmail.com>
2018-10-28 11:05:14 -0300

    inclusao da formatacao dos dados dos pilotos

--- src/br/com/gympass/RacingReport.java
+++ src/br/com/gympass/RacingReport.java
@@ -1,5 +1,9 @@
 package br.com.gympass;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class RacingReport {
@@ -28,8 +32,64 @@ public class RacingReport {
     }
 
     private void format(final Pilot pilot) {
-        // TODO Auto-generated method stub
+        formatFinishingPosition(pilot);
+        formatCode(pilot);
+        formatName(pilot);
+        formatLastLap(pilot);
+        formatTotalTime(pilot);
 
+        builder.append('\n');
+    }
+
+    private void formatTotalTime(final Pilot pilot) {
+        final Calendar calendar = GregorianCalendar.getInstance();
+        calendar.setTimeInMillis(pilot.getTotalRacingTime());
+        final Date date = calendar.getTime();
+
+        final SimpleDateFormat format = new SimpleDateFormat("mm:ss.SSS");
+        final String totalTimeString = format.format(date);
+
+        builder.append(totalTimeString);
+
+        for (int i = totalTimeString.length(); i < 11; ++i) {
+            builder.append(' ');
+        }
+    }
+
+    private void formatLastLap(final Pilot pilot) {
+        final String lastLapString = Integer.toString(pilot.getLastLap());
+        builder.append(lastLapString);
+
+        for (int i = lastLapString.length(); i < 26; ++i) {
+            builder.append(' ');
+        }
+    }
+
+    private void formatName(final Pilot pilot) {
+        final String name = pilot.getName();
+        builder.append(name);
+
+        for (int i = name.length(); i < 14; ++i) {
+            builder.append(' ');
+        }
+    }
+
+    private void formatCode(final Pilot pilot) {
+        final String codeString = Integer.toString(pilot.getCode());
+        builder.append(codeString);
+
+        for (int i = codeString.length(); i < 16; ++i) {
+            builder.append(' ');
+        }
+    }
+
+    private void formatFinishingPosition(final Pilot pilot) {
+        final String finishingPositionString = Integer.toString(pilot.getFinishingPosition());
+        builder.append(finishingPositionString);
+
+        for (int i = finishingPositionString.length(); i < 18; ++i) {
+            builder.append(' ');
+        }
     }
 
     public String getReport() {
--- test/br/com/gympass/RacingReportTest.java
+++ test/br/com/gympass/RacingReportTest.java
@@ -1,13 +1,86 @@
 package br.com.gympass;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RacingReportTest {
 
     private static final String EMPTY_REPORT = "Posição Chegada   Código Piloto   Nome Piloto   Qtde Voltas Completadas   Tempo Total\n";
+    private static final String ONE_PILOT_REPORT = "Posição Chegada   Código Piloto   Nome Piloto   Qtde Voltas Completadas   Tempo Total\n"
+            + "1                 38              F.MASSA       4                         01:03.170  \n";
+    private static final String TWO_PILOT_REPORT = "Posição Chegada   Código Piloto   Nome Piloto   Qtde Voltas Completadas   Tempo Total\n"
+            + "1                 38              F.MASSA       4                         01:03.170  \n"
+            + "2                 33              R.BARRICHELLO 4                         01:04.352  \n";
+    private Pilot pilot1;
+    private Pilot pilot2;
+
+    @Before
+    public void createPilots() {
+        pilot1 = new Pilot();
+        pilot1.setName("F.MASSA");
+        pilot1.setFinishingPosition(1);
+        pilot1.setCode(38);
+        pilot1.setLastLap(4);
+        pilot1.setTotalRacingTime(63170);
+
+        Lap lap = new Lap();
+        lap.setNumber(1);
+        lap.setTime(62852);
+        pilot1.put(lap);
+
+        lap = new Lap();
+        lap.setNumber(2);
+        lap.setTime(63170);
+        pilot1.put(lap);
+
+        pilot2 = new Pilot();
+        pilot2.setName("R.BARRICHELLO");
+        pilot2.setFinishingPosition(2);
+        pilot2.setCode(33);
+        pilot2.setLastLap(4);
+        pilot2.setTotalRacingTime(64352);
+
+        lap = new Lap();
+        lap.setNumber(1);
+        lap.setTime(64352);
+        pilot2.put(lap);
+
+        lap = new Lap();
+        lap.setNumber(2);
+        lap.setTime(64002);
+        pilot2.put(lap);
+    }
+
+    @Test
+    public void testcreateReportTwoPilot() {
+        final List<Pilot> pilots = new ArrayList<>();
+        pilots.add(pilot1);
+        pilots.add(pilot2);
+
+        final RacingReport racingReport = new RacingReport();
+        racingReport.setPilots(pilots);
+        racingReport.createReport();
+        final String report = racingReport.getReport();
+
+        Assert.assertEquals(TWO_PILOT_REPORT, report);
+    }
+
+    @Test
+    public void testcreateReportOnePilot() {
+        final List<Pilot> pilots = new ArrayList<>();
+        pilots.add(pilot1);
+
+        final RacingReport racingReport = new RacingReport();
+        racingReport.setPilots(pilots);
+        racingReport.createReport();
+        final String report = racingReport.getReport();
+
+        Assert.assertEquals(ONE_PILOT_REPORT, report);
+    }
 
     @Test
     public void testcreateReportEmpty() {
